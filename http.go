ced48613e412f345d91f05609ced63020d9a673a
a1yamino <bellaster@qq.com>
2023-10-30 21:53:58 +0800

    implemented http

---
+++ http.go
@@ -0,0 +1,56 @@
+package goCacheDemo
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+)
+
+const defaultBashPath = "/gocachedemo/"
+
+type HTTPPool struct {
+	self     string
+	basePath string
+}
+
+func NewHTTPPool(self string) *HTTPPool {
+	return &HTTPPool{
+		self:     self,
+		basePath: defaultBashPath,
+	}
+}
+
+func (p *HTTPPool) Log(format string, a ...interface{}) {
+	log.Printf("[Server %s] %s", p.self, fmt.Sprintf(format, a...))
+}
+
+func (p *HTTPPool) ServerHTTP(w http.ResponseWriter, r *http.Request) {
+	if !strings.HasPrefix(r.URL.Path, p.basePath) {
+		panic("HTTPPool serving unexpected path: " + r.URL.Path)
+	}
+	p.Log("%s %s", r.Method, r.URL.Path)
+	parts := strings.SplitN(r.URL.Path[len(p.basePath):], "/", 2)
+	if len(parts) != 2 {
+		http.Error(w, "Invalid path", http.StatusBadRequest)
+		return
+	}
+
+	groupName := parts[0]
+	key := parts[1]
+
+	group := GetGroup(groupName)
+	if group == nil {
+		http.Error(w, "no such group:"+groupName, http.StatusNotFound)
+		return
+	}
+
+	view, err := group.Get(key)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/octet-stream")
+	w.Write(view.ByteSlice())
+}
